afda143c242382c4d816eb8c9254b0f4a6947efd
rajneesh.k.anand@gmail.com <rajneesh.k.anand@gmail.com>
2022-08-15 23:06:20 +0530

    person api route get order by

--- controllers/register-person.js
+++ controllers/register-person.js
@@ -66,7 +66,13 @@ const registerPerson = async (req, res) => {
 
 const fetchPerson = async (req, res) => {
   try {
-    const result = await prisma.person.findMany();
+    const result = await prisma.person.findMany({
+      orderBy: [
+        {
+          id: "asc",
+        },
+      ],
+    });
     return res.status(200).json({
       msg: "success",
       data: result,
